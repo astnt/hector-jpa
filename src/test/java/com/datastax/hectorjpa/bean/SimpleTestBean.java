29b13345c6fde7971af6e4cd5708a33fec024ada
zznate <nate@vervewireless.com>
2011-03-03 16:51:21 -0600

    initial import of working (sorta) PoC

---
+++ src/test/java/com/datastax/hectorjpa/bean/SimpleTestBean.java
@@ -0,0 +1,55 @@
+package com.datastax.hectorjpa.bean;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="SimpleTestBeanColumnFamily")
+public class SimpleTestBean implements Serializable {
+  
+  private long id;
+  private String name;
+  
+  public SimpleTestBean() {    
+  }
+  
+  public SimpleTestBean(long id, String name) {
+    this.id = id;
+    this.name = name;
+  }
+  
+  @Id
+  public long getId() {
+    return id;
+  }
+  
+  public void setId(long id) {
+    this.id = id;
+  }
+  
+  @Column(name="name")
+  public String getName() {
+    return name;
+  }
+  
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return new StringBuilder(256)
+    .append("name:")
+    .append(getName())
+    .append(",id:")
+    .append(getId())
+    .toString();
+  }
+  
+  
+
+}
---
+++ src/test/java/com/datastax/hectorjpa/store/StandaloneEntityManagerFactoryTest.java
@@ -0,0 +1,39 @@
+package com.datastax.hectorjpa.store;
+
+import static org.junit.Assert.*;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import com.datastax.hectorjpa.bean.SimpleTestBean;
+
+public class StandaloneEntityManagerFactoryTest extends ManagedEntityTestBase {
+  
+  @Test
+  public void testBuildEntityManagerFactory() {
+    
+    EntityManager em = entityManagerFactory.createEntityManager();
+    em.getTransaction().begin();
+    em.persist(new SimpleTestBean(1, "foo"));
+    em.getTransaction().commit();
+    em.close();
+    
+    //em.getTransaction().begin();
+    
+    EntityManager em2 = entityManagerFactory.createEntityManager();
+    SimpleTestBean stb = em2.find(SimpleTestBean.class, 1);
+    //em.getTransaction().commit();
+    assertEquals("foo",stb.getName());
+    
+
+    em2.getTransaction().begin();
+    em2.remove(stb);
+    em2.getTransaction().commit();
+    em2.close();
+  }  
+
+}
