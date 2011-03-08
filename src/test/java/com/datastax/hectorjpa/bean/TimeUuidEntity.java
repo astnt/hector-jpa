e4deb7bc389d37cf900d778b79bcdc60a4bbb852
toddnine <todd@spidertracks.co.nz>
2011-03-08 17:24:10 +1300

    Updated to version 2.1.0 of Open JPA.  Also updated persistence.xml to include TimeUuidEntity.

--- src/test/java/com/datastax/hectorjpa/bean/TimeUuidEntity.java
+++ src/test/java/com/datastax/hectorjpa/bean/TimeUuidEntity.java
@@ -26,7 +26,7 @@ public abstract class TimeUuidEntity {
 
 	@Id
 	@Persistent
-	@GeneratedValue(generator="timeuuid", strategy=GenerationType.IDENTITY)
+	@GeneratedValue(generator="timeuuid", strategy=GenerationType.SEQUENCE)
 	private UUID id;
 	
 	
@@ -38,37 +38,39 @@ public abstract class TimeUuidEntity {
 	}
 
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#hashCode()
-	 */
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#hashCode()
+   */
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + ((id == null) ? 0 : id.hashCode());
+    return result;
+  }
 
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#equals(java.lang.Object)
-	 */
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		TimeUuidEntity other = (TimeUuidEntity) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+  /* (non-Javadoc)
+   * @see java.lang.Object#equals(java.lang.Object)
+   */
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (!(obj instanceof TimeUuidEntity))
+      return false;
+    TimeUuidEntity other = (TimeUuidEntity) obj;
+    if (id == null) {
+      if (other.id != null)
+        return false;
+    } else if (!id.equals(other.id))
+      return false;
+    return true;
+  }
+
+
 
 	//no set id intentionally, we want the framework to set it the first time this entity is saved
 
--- src/test/java/com/datastax/hectorjpa/store/ManyToManyIndexTest.java
+++ src/test/java/com/datastax/hectorjpa/store/ManyToManyIndexTest.java
@@ -1,9 +1,11 @@
 package com.datastax.hectorjpa.store;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityNotFoundException;
 
 import org.junit.Test;
 
@@ -19,43 +21,52 @@ import com.datastax.hectorjpa.bean.User;
  */
 public class ManyToManyIndexTest extends ManagedEntityTestBase {
 
-	/**
-	 * Test simple instance with no collections to ensure we persist properly without indexing
-	 */
-	@Test
-	public void basicCasePersistence() {
+  /**
+   * Test simple instance with no collections to ensure we persist properly
+   * without indexing
+   */
+  @Test
+  public void basicCasePersistence() {
 
-		EntityManager em = entityManagerFactory.createEntityManager();
-		em.getTransaction().begin();
+    EntityManager em = entityManagerFactory.createEntityManager();
+    em.getTransaction().begin();
 
-		User user = new User();
-		user.setFirstName("Test");
-		user.setLastName("User");
-		user.setEmail("test.user@testing.com");
+    User user = new User();
+    user.setFirstName("Test");
+    user.setLastName("User");
+    user.setEmail("test.user@testing.com");
 
-		em.persist(user);
-		em.getTransaction().commit();
-		em.close();
+    em.persist(user);
+    em.getTransaction().commit();
+    em.close();
 
-		EntityManager em2 = entityManagerFactory.createEntityManager();
-		User returnedUser = em2.find(User.class, user.getId());
+    EntityManager em2 = entityManagerFactory.createEntityManager();
+    User returnedUser = em2.find(User.class, user.getId());
 
-		assertEquals(user, returnedUser);
+    assertEquals(user, returnedUser);
 
-		assertEquals(user.getFirstName(), returnedUser.getFirstName());
-		assertEquals(user.getLastName(), returnedUser.getLastName());
-		assertEquals(user.getEmail(), returnedUser.getEmail());
+    assertEquals(user.getFirstName(), returnedUser.getFirstName());
+    assertEquals(user.getLastName(), returnedUser.getLastName());
+    assertEquals(user.getEmail(), returnedUser.getEmail());
 
-		em2.getTransaction().begin();
+    em2.getTransaction().begin();
 
-		em2.remove(returnedUser);
-		em2.getTransaction().commit();
-		em2.close();
+    em2.remove(returnedUser);
+    em2.getTransaction().commit();
+    em2.close();
 
-		returnedUser = em2.find(User.class, user.getId());
+    EntityManager em3 = entityManagerFactory.createEntityManager();
 
-		assertNull(returnedUser);
+    boolean found = true;
 
-	}
+    try {
+      em3.find(User.class, user.getId());
+    } catch (EntityNotFoundException enfe) {
+      found = false;
+    }
+
+    assertFalse(found);
+
+  }
 
 }
