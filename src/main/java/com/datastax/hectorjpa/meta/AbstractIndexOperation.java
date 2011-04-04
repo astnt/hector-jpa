00e65095ee706bca75920c1ee67aa9fea98dac79
toddnine <todd@spidertracks.co.nz>
2011-04-04 17:40:32 +1200

    Added name for reverse indexing.  Need add the reverse index write and clean up op.

--- src/main/java/com/datastax/hectorjpa/meta/AbstractIndexOperation.java
+++ src/main/java/com/datastax/hectorjpa/meta/AbstractIndexOperation.java
@@ -42,325 +42,344 @@ import com.datastax.hectorjpa.store.MappingUtils;
  */
 public abstract class AbstractIndexOperation {
 
-	public static final String CF_NAME = "Index_Container";
+  public static final String CF_NAME = "Index_Container";
 
-	protected static byte[] HOLDER = new byte[] { 0 };
+  protected static byte[] HOLDER = new byte[] { 0 };
 
-	protected static final StringSerializer stringSerializer = StringSerializer
-			.get();
+  protected static final StringSerializer stringSerializer = StringSerializer
+      .get();
 
-	protected static final DynamicCompositeSerializer compositeSerializer = new DynamicCompositeSerializer();
+  protected static final DynamicCompositeSerializer compositeSerializer = new DynamicCompositeSerializer();
 
-	protected static final BytesArraySerializer bytesSerializer = BytesArraySerializer
-			.get();
+  protected static final BytesArraySerializer bytesSerializer = BytesArraySerializer
+      .get();
 
-	protected static int MAX_SIZE = 500;
+  protected static int MAX_SIZE = 500;
 
-	/**
-	 * the byte value for the row key of our index
-	 */
-	protected byte[] indexName;
+  /**
+   * the byte value for the row key of our index
+   */
+  protected byte[] indexName;
 
-	protected QueryIndexField[] fields;
+  /**
+   * The bytes that represent the reverse index
+   */
+  protected byte[] reverseIndexName;
 
-	protected QueryOrderField[] orders;
+  protected QueryIndexField[] fields;
 
-	protected IndexDefinition indexDefinition;
+  protected QueryOrderField[] orders;
 
-	protected Serializer<Object> idSerializer;
+  protected IndexDefinition indexDefinition;
 
-	public AbstractIndexOperation(CassandraClassMetaData metaData,
-			IndexDefinition indexDef) {
-		this.indexDefinition = indexDef;
+  protected Serializer<Object> idSerializer;
 
-		FieldOrder[] fieldDirections = indexDef.getIndexedFields();
-		IndexOrder[] indexOrders = indexDef.getOrderFields();
+  public AbstractIndexOperation(CassandraClassMetaData metaData,
+      IndexDefinition indexDef) {
+    this.indexDefinition = indexDef;
 
-		this.fields = new QueryIndexField[fieldDirections.length];
-		this.orders = new QueryOrderField[indexOrders.length];
+    FieldOrder[] fieldDirections = indexDef.getIndexedFields();
+    IndexOrder[] indexOrders = indexDef.getOrderFields();
 
-		ByteBufferOutputStream outputStream = new ByteBufferOutputStream();
+    this.fields = new QueryIndexField[fieldDirections.length];
+    this.orders = new QueryOrderField[indexOrders.length];
 
-		FieldMetaData fmd = null;
+    ByteBufferOutputStream searchIndexNameBuff = new ByteBufferOutputStream();
+    ByteBufferOutputStream reverseIndexNameBuff = new ByteBufferOutputStream();
 
-		for (int i = 0; i < fieldDirections.length; i++) {
-			fmd = metaData.getField(fieldDirections[i].getName());
+    FieldMetaData fmd = null;
 
-			if (fmd == null) {
-				throw new MetaDataException(
-						String.format(
-								"You specified field '%s' as an index field, yet it does not exist in class '%s'",
-								fieldDirections[i].getName(),
-								metaData.getDescribedType()));
-			}
+    for (int i = 0; i < fieldDirections.length; i++) {
+      fmd = metaData.getField(fieldDirections[i].getName());
 
-			fields[i] = new QueryIndexField(fmd);
+      if (fmd == null) {
+        throw new MetaDataException(
+            String
+                .format(
+                    "You specified field '%s' as an index field, yet it does not exist in class '%s'",
+                    fieldDirections[i].getName(), metaData.getDescribedType()));
+      }
 
-			outputStream.write(stringSerializer.toByteBuffer(fieldDirections[i]
-					.getName()));
+      fields[i] = new QueryIndexField(fmd);
 
-		}
+      searchIndexNameBuff.write(stringSerializer.toByteBuffer(fieldDirections[i]
+          .getName()));
+      reverseIndexNameBuff.write(stringSerializer.toByteBuffer(fieldDirections[i]
+          .getName()));
 
-		for (int i = 0; i < indexOrders.length; i++) {
-			fmd = metaData.getField(indexOrders[i].getName());
+    }
 
-			if (fmd == null) {
-				throw new MetaDataException(
-						String.format(
-								"You specified field '%s' as an order field, yet it does not exist in class '%s'",
-								indexOrders[i].getName(),
-								metaData.getDescribedType()));
-			}
+    for (int i = 0; i < indexOrders.length; i++) {
+      fmd = metaData.getField(indexOrders[i].getName());
 
-			orders[i] = new QueryOrderField(indexOrders[i], fmd);
+      if (fmd == null) {
+        throw new MetaDataException(
+            String
+                .format(
+                    "You specified field '%s' as an order field, yet it does not exist in class '%s'",
+                    indexOrders[i].getName(), metaData.getDescribedType()));
+      }
 
-			outputStream.write(stringSerializer.toByteBuffer(indexOrders[i]
-					.getName()));
-		}
+      orders[i] = new QueryOrderField(indexOrders[i], fmd);
 
-		ByteBuffer result = outputStream.getByteBuffer();
+      searchIndexNameBuff.write(stringSerializer.toByteBuffer(indexOrders[i]
+          .getName()));
+      reverseIndexNameBuff.write(stringSerializer.toByteBuffer(indexOrders[i]
+          .getName()));
+    }
 
-		indexName = new byte[result.limit() - result.position()];
+    
+    searchIndexNameBuff.write(stringSerializer.toByteBuffer("search"));
+    reverseIndexNameBuff.write(stringSerializer.toByteBuffer("reverse"));
+    
+    ByteBuffer result = searchIndexNameBuff.getByteBuffer();
 
-		result.get(indexName);
+    indexName = new byte[result.limit() - result.position()];
 
-		// now get our is serializer
-		this.idSerializer = MappingUtils.getSerializerForPk(metaData);
+    result.get(indexName);
+    
+    
+    result = reverseIndexNameBuff.getByteBuffer();
 
-		// if the ID doesn't implement comparable, we can't compare our results
-		if (!Comparable.class
-				.isAssignableFrom(metaData.getPrimaryKeyFields()[0]
-						.getDeclaredType())) {
-			throw new MetaDataException(
-					String.format(
-							"Ids for indexes objects must implement Comparable.  Field '%s' on class '%s' does not implement comparable",
-							metaData.getPrimaryKeyFields()[0].getName(),
-							metaData.getDescribedType()));
-		}
+    this.reverseIndexName = new byte[result.limit() - result.position()];
 
-	}
+    result.get(this.reverseIndexName);
 
-	/**
-	 * Write the index
-	 * 
-	 * @param stateManager
-	 * @param mutator
-	 * @param clock
-	 */
-	public abstract void writeIndex(OpenJPAStateManager stateManager,
-			Mutator<byte[]> mutator, long clock);
+    // now get our is serializer
+    this.idSerializer = MappingUtils.getSerializerForPk(metaData);
 
-	/**
-	 * Scan the given index query and add the results to the provided set. The
-	 * set comparator of the dynamic columns are compared via a tree comparator
-	 * 
-	 * @param query
-	 */
-	public abstract void scanIndex(IndexQuery query,
-			Set<DynamicComposite> results, Keyspace keyspace);
+    // if the ID doesn't implement comparable, we can't compare our results
+    if (!Comparable.class.isAssignableFrom(metaData.getPrimaryKeyFields()[0]
+        .getDeclaredType())) {
+      throw new MetaDataException(
+          String
+              .format(
+                  "Ids for indexes objects must implement Comparable.  Field '%s' on class '%s' does not implement comparable",
+                  metaData.getPrimaryKeyFields()[0].getName(),
+                  metaData.getDescribedType()));
+    }
 
-	/**
-	 * Construct the 2 composites from the fields in this index. Returns true if
-	 * the index values have changed.
-	 * 
-	 * @param newComposite
-	 * @param oldComposite
-	 * @return
-	 */
-	protected boolean constructComposites(DynamicComposite newComposite,
-			DynamicComposite oldComposite, OpenJPAStateManager stateManager) {
+  }
 
-		boolean changed = false;
+  /**
+   * Write the index
+   * 
+   * @param stateManager
+   * @param mutator
+   * @param clock
+   */
+  public abstract void writeIndex(OpenJPAStateManager stateManager,
+      Mutator<byte[]> mutator, long clock);
 
-		Object key = MappingUtils.getTargetObject(stateManager.getObjectId());
+  /**
+   * Scan the given index query and add the results to the provided set. The set
+   * comparator of the dynamic columns are compared via a tree comparator
+   * 
+   * @param query
+   */
+  public abstract void scanIndex(IndexQuery query,
+      Set<DynamicComposite> results, Keyspace keyspace);
 
-		Object field;
+  /**
+   * Construct the 2 composites from the fields in this index. Returns true if
+   * the index values have changed.
+   * 
+   * @param newComposite
+   * @param oldComposite
+   * @return
+   */
+  protected boolean constructComposites(DynamicComposite newComposite,
+      DynamicComposite oldComposite, OpenJPAStateManager stateManager) {
 
-		// now construct the composite with order by the ids at the end.
-		for (QueryIndexField indexField : fields) {
-			field = indexField.getValue(stateManager,
-					stateManager.getPersistenceCapable());
+    //TODO TN add reverse index writes
+    
+    boolean changed = false;
 
-			// add this to all deletes for the order composite.
-			indexField.addFieldWrite(newComposite, field);
+    Object key = MappingUtils.getTargetObject(stateManager.getObjectId());
 
-			// The deletes to teh is composite
-			changed |= indexField.addFieldDelete(oldComposite, field);
-		}
+    Object field;
 
-		// now construct the composite with order by the ids at the end.
-		for (AbstractIndexField order : orders) {
+    // now construct the composite with order by the ids at the end.
+    for (QueryIndexField indexField : fields) {
+      field = indexField.getValue(stateManager,
+          stateManager.getPersistenceCapable());
 
-			// get the field value from our current index
-			field = order.getValue(stateManager,
-					stateManager.getPersistenceCapable());
+      // add this to all deletes for the order composite.
+      indexField.addFieldWrite(newComposite, field);
 
-			// add this to all deletes for the order composite.
-			order.addFieldWrite(newComposite, field);
+      // The deletes to teh is composite
+      changed |= indexField.addFieldDelete(oldComposite, field);
+    }
 
-			// The deletes to teh is composite
-			changed |= order.addFieldDelete(oldComposite, field);
-		}
+    // now construct the composite with order by the ids at the end.
+    for (AbstractIndexField order : orders) {
 
-		// add it to our new value
+      // get the field value from our current index
+      field = order
+          .getValue(stateManager, stateManager.getPersistenceCapable());
 
-		newComposite.addComponent(key, idSerializer);
+      // add this to all deletes for the order composite.
+      order.addFieldWrite(newComposite, field);
 
-		oldComposite.addComponent(key, idSerializer);
+      // The deletes to teh is composite
+      changed |= order.addFieldDelete(oldComposite, field);
+    }
 
-		return changed;
-	}
+    // add it to our new value
 
-	/**
-	 * Execute a query with the given start and end dynamic composites
-	 * 
-	 * @param start
-	 *            The start value from the range scan
-	 * @param end
-	 *            The end value in the range scan
-	 * @param results
-	 *            The results to add the returned values to. Sorted by order
-	 *            fields, then id
-	 * @param keyspace
-	 *            The kesypace
-	 */
-	protected void executeQuery(DynamicComposite start, DynamicComposite end,
-			Set<DynamicComposite> results, Keyspace keyspace) {
+    newComposite.addComponent(key, idSerializer);
 
-		SliceQuery<byte[], DynamicComposite, byte[]> sliceQuery = new ThriftSliceQuery(
-				keyspace, BytesArraySerializer.get(), compositeSerializer,
-				BytesArraySerializer.get());
+    oldComposite.addComponent(key, idSerializer);
 
-		DynamicComposite startScan = start;
-		QueryResult<ColumnSlice<DynamicComposite, byte[]>> result = null;
+    return changed;
+  }
 
-		do {
+  /**
+   * Execute a query with the given start and end dynamic composites
+   * 
+   * @param start
+   *          The start value from the range scan
+   * @param end
+   *          The end value in the range scan
+   * @param results
+   *          The results to add the returned values to. Sorted by order fields,
+   *          then id
+   * @param keyspace
+   *          The kesypace
+   */
+  protected void executeQuery(DynamicComposite start, DynamicComposite end,
+      Set<DynamicComposite> results, Keyspace keyspace) {
 
-			sliceQuery.setRange(startScan, end, false, MAX_SIZE);
-			sliceQuery.setKey(indexName);
-			sliceQuery.setColumnFamily(CF_NAME);
+    SliceQuery<byte[], DynamicComposite, byte[]> sliceQuery = new ThriftSliceQuery(
+        keyspace, BytesArraySerializer.get(), compositeSerializer,
+        BytesArraySerializer.get());
 
-			result = sliceQuery.execute();
+    DynamicComposite startScan = start;
+    QueryResult<ColumnSlice<DynamicComposite, byte[]>> result = null;
 
-			for (HColumn<DynamicComposite, byte[]> col : result.get()
-					.getColumns()) {
-				start = col.getName();
-				results.add(start);
-			}
+    do {
 
-		} while (result.get().getColumns().size() == MAX_SIZE);
-	}
+      sliceQuery.setRange(startScan, end, false, MAX_SIZE);
+      sliceQuery.setKey(indexName);
+      sliceQuery.setColumnFamily(CF_NAME);
 
-	/**
-	 * We can only write non 0 separators at the last value in our composite type
-	 * @param orig
-	 * @param index
-	 * @param length
-	 * @return
-	 */
-  protected ComponentEquality getEquality(ComponentEquality orig, int index, int length){
-    //not the last value in the scan range, so we reset it from trailing 1 bytes on the slice column to a 0 byte
-    if(index != length -1){
+      result = sliceQuery.execute();
+
+      for (HColumn<DynamicComposite, byte[]> col : result.get().getColumns()) {
+        start = col.getName();
+        results.add(start);
+      }
+
+    } while (result.get().getColumns().size() == MAX_SIZE);
+  }
+
+  /**
+   * We can only write non 0 separators at the last value in our composite type
+   * 
+   * @param orig
+   * @param index
+   * @param length
+   * @return
+   */
+  protected ComponentEquality getEquality(ComponentEquality orig, int index,
+      int length) {
+    // not the last value in the scan range, so we reset it from trailing 1
+    // bytes on the slice column to a 0 byte
+    if (index != length - 1) {
       return ComponentEquality.EQUAL;
     }
-    
+
     return orig;
-    
+
   }
-  
 
-	public Comparator<DynamicComposite> getComprator() {
-		return new ResultComparator();
-	}
+  public Comparator<DynamicComposite> getComprator() {
+    return new ResultComparator();
+  }
 
-	/**
-	 * @return the indexDefinition
-	 */
-	public IndexDefinition getIndexDefinition() {
-		return indexDefinition;
-	}
+  /**
+   * @return the indexDefinition
+   */
+  public IndexDefinition getIndexDefinition() {
+    return indexDefinition;
+  }
 
-	public class ResultComparator implements Comparator<DynamicComposite> {
+  public class ResultComparator implements Comparator<DynamicComposite> {
 
-		@Override
-		public int compare(DynamicComposite c1, DynamicComposite c2) {
+    @Override
+    public int compare(DynamicComposite c1, DynamicComposite c2) {
 
-		  
-		  if(c1==null && c2 != null){
-		    return -1;
-		  }
-		  
-		  if(c2 == null && c1 != null){
-		    return 1;
-		  }
-		  
-		  if(c1 == null && c2 == null){
-		    return 0;
-		  }
-		  
-			int compare = 0;
-			
-			int size = 0;
-			
-			Comparable<Object> c1Id = null;
-			Comparable<Object> c2Id = null;
-			
-			
-			// no order by, just order by each field starting from the bginning
-			if (orders.length == 0) {
-			  
-			  size = fields.length;
-			  
-				for(int i = 0; i < size; i ++){
-					c1Id = (Comparable<Object>) c1.get(i, fields[i].getSerializer());
+      if (c1 == null && c2 != null) {
+        return -1;
+      }
 
-					c2Id = (Comparable<Object>) c2.get(i, fields[i].getSerializer());
+      if (c2 == null && c1 != null) {
+        return 1;
+      }
 
-					compare =  c1Id.compareTo(c2Id);
-					
-					if(compare != 0){
-						return compare;
-					}
-				}
-				
-				c1Id = (Comparable<Object>) c1.get(c1.size()-1, idSerializer);
+      if (c1 == null && c2 == null) {
+        return 0;
+      }
 
-				c2Id = (Comparable<Object>) c2.get(c2.size()-1, idSerializer);
+      int compare = 0;
 
-				return c1Id.compareTo(c2Id);
-				
-			}
+      int size = 0;
 
-			size = c1.getComponents().size();
-			
-			int c1StartIndex = size - orders.length - 1; // c1.getComponents().size()
-			// -
-			// orders.length-2;
-			int c2StartIndex = size - orders.length - 1; // c1.getComponents().size()
-			// -
-			// orders.length-2;
+      Comparable<Object> c1Id = null;
+      Comparable<Object> c2Id = null;
 
-			for (int i = 0; i < orders.length; i++) {
+      // no order by, just order by each field starting from the bginning
+      if (orders.length == 0) {
 
-				compare = orders[i].compare(c1, c1StartIndex + i, c2,
-						c2StartIndex + i);
+        size = fields.length;
 
-				if (compare != 0) {
-					return compare;
-				}
-			}
+        for (int i = 0; i < size; i++) {
+          c1Id = (Comparable<Object>) c1.get(i, fields[i].getSerializer());
 
-			// if we get here the compare fields are equal. Compare ids
-			c1Id = (Comparable<Object>) c1.get(size-1, idSerializer);
+          c2Id = (Comparable<Object>) c2.get(i, fields[i].getSerializer());
 
-			c2Id = (Comparable<Object>) c2.get(size-1, idSerializer);
+          compare = c1Id.compareTo(c2Id);
 
-			return c1Id.compareTo(c2Id);
+          if (compare != 0) {
+            return compare;
+          }
+        }
 
-		}
-		
-	
+        c1Id = (Comparable<Object>) c1.get(c1.size() - 1, idSerializer);
 
-	}
+        c2Id = (Comparable<Object>) c2.get(c2.size() - 1, idSerializer);
+
+        return c1Id.compareTo(c2Id);
+
+      }
+
+      size = c1.getComponents().size();
+
+      int c1StartIndex = size - orders.length - 1; // c1.getComponents().size()
+      // -
+      // orders.length-2;
+      int c2StartIndex = size - orders.length - 1; // c1.getComponents().size()
+      // -
+      // orders.length-2;
+
+      for (int i = 0; i < orders.length; i++) {
+
+        compare = orders[i].compare(c1, c1StartIndex + i, c2, c2StartIndex + i);
+
+        if (compare != 0) {
+          return compare;
+        }
+      }
+
+      // if we get here the compare fields are equal. Compare ids
+      c1Id = (Comparable<Object>) c1.get(size - 1, idSerializer);
+
+      c2Id = (Comparable<Object>) c2.get(size - 1, idSerializer);
+
+      return c1Id.compareTo(c2Id);
+
+    }
+
+  }
 
 }
