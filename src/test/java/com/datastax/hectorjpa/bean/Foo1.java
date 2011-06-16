360a104da1960b62df5feb96edeb5d64f4600f07
Patrick Julien <pjulien@gmail.com>
2011-06-16 16:14:24 -0400

    Test case for issue #7

---
+++ src/test/java/com/datastax/hectorjpa/bean/Foo1.java
@@ -0,0 +1,37 @@
+package com.datastax.hectorjpa.bean;
+
+import com.datastax.hectorjpa.annotation.ColumnFamily;
+import com.datastax.hectorjpa.annotation.Index;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+
+@Entity
+@ColumnFamily("Foo1ColumnFamily")
+@Index(fields = "other")
+@NamedQueries({ @NamedQuery(name = "byother", query = "select t from Foo1 as t where t.other >= :otherLow and t.other < :otherHigh") })
+public class Foo1 {
+    @Id
+    @GeneratedValue
+    private Long id;
+    private Integer other;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Integer getOther() {
+        return other;
+    }
+
+    public void setOther(Integer other) {
+        this.other = other;
+    }
+}
